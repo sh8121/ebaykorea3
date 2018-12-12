91cc4b5582a1c09c5be4e39db1eb7f97656af2af
urstory <urstory@gmail.com>
2018-12-12 16:47:35 +0900

    fix code

--- shop/src/main/java/kr/co/sunnyvale/shop/config/WebSecurityConfig.java
+++ shop/src/main/java/kr/co/sunnyvale/shop/config/WebSecurityConfig.java
@@ -1,8 +1,6 @@
 package kr.co.sunnyvale.shop.config;
 
 
-import kr.co.sunnyvale.shop.security.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -12,9 +10,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    CustomUserDetailsService customUserDetailsService;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -30,24 +25,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .permitAll().and()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/")
+                    .permitAll().and()
                 .authorizeRequests()
-                .antMatchers("/errors/**").permitAll()
-                .antMatchers("/").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/members/login").permitAll()
-                .antMatchers("/members/joinform").permitAll()
-                .antMatchers("/members/join").permitAll()
-               .antMatchers("/users/**").hasRole("MEMBER")
-                .antMatchers("/api/**").hasRole("MEMBER")
-                .anyRequest().fullyAuthenticated().and()
+                    .antMatchers("/errors/**").permitAll()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .antMatchers("/members/login").permitAll()
+                    .antMatchers("/members/joinform").permitAll()
+                    .antMatchers("/members/join").permitAll()
+                    .antMatchers("/members/**").hasRole("MEMBER")
+                    .antMatchers("/api/**").hasRole("MEMBER")
+                    .anyRequest().fullyAuthenticated().and()
                 .formLogin()
-                .loginPage("/members/login")
-                .usernameParameter("id").passwordParameter("password")
-                .loginProcessingUrl("/members/login")
-                .failureUrl("/members/login");
+                    .loginPage("/members/login")
+                    .usernameParameter("id").passwordParameter("password")
+                    .loginProcessingUrl("/members/login")
+                    .failureUrl("/members/login");
     }
 }
 
--- shop/src/main/java/kr/co/sunnyvale/shop/service/MemberService.java
+++ shop/src/main/java/kr/co/sunnyvale/shop/service/MemberService.java
@@ -4,12 +4,14 @@ import kr.co.sunnyvale.shop.domain.Member;
 import kr.co.sunnyvale.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Transactional(readOnly = true)
     public Member getMember(String email){
         return memberRepository.getMember(email);
     }
